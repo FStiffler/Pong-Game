d07ee74875a44a5ab73790deecd813d9aa53df40
FStiffler <fstiffler@gmx.ch>
2021-12-06 17:44:58 +0100

    Defined function to control ball movement in new file called helperFunctions

---
+++ pong_game/helperFunctions.py
@@ -0,0 +1,27 @@
+# Define a function to control movement of ball
+def ball_movement(x, y, height, ball):
+    '''
+    x (int): Integer defining the movement direction on x axis
+    y (int): Integer defining the movement direction on y axis
+    height (int): Defines the height of the playing window
+    width (int): Defines the width of the playing window
+    ball (Ball): The ball object of class Ball
+
+    Returns:
+    y_invert (int): Inverted movement in y-axis direction when ball hits frame
+    '''
+
+    # New Ball position
+    ball.set_position(x, y)
+
+    # If ball touches upper frame
+    if ball.get_position()[1] + ball.get_size() == height:
+        # invert y movement
+        y_invert = y * -1
+        return y_invert
+
+    # If ball touches lower frame
+    if ball.get_position()[1] - ball.get_size() == 0:
+        # invert y movement
+        y_invert = y * -1
+        return y_invert
